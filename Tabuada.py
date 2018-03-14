9c71ae11784b108fbd8669a843a3aca5fd0512c9
guile.hm <guile.hm@hotmail.com>
2018-03-14 09:53:43 -0300

    Improvements

--- Tabuada.py
+++ Tabuada.py
@@ -1,4 +1,6 @@
 # Tabuada em uma linha
 
 tabuada = [(a, b, a * b) for a in range(2, 3) for b in range(1, 11)]
-print(tabuada)
+# print(tabuada)
+
+print("\n".join("{} x {} = {}".format(a, b, a * b) for a in range(1, 11) for b in range(1, 11)))
