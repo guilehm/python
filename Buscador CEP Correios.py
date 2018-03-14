ed603f2e79ad03fcdd0fce647c6a92e30e5842e4
guile.hm <guile.hm@hotmail.com>
2018-03-14 09:36:18 -0300

    Requests

---
+++ Buscador CEP Correios.py
@@ -0,0 +1,11 @@
+import requests
+from lxml import etree
+
+URL = 'http://www.buscacep.correios.com.br/sistemas/buscacep/resultadoBuscaCepEndereco.cfm'
+
+def busca_cep(cep):
+    r = requests.POST(URL, data=dict(relaxation=cep, tipoCEP='ALL',  semelhante='N'))
+    tabela = etree.HTML(r.text).find(".//table[@class='tmptabela']")
+    logradouro, bairro, cidade_uf, cep = (x.text.strip() for x in tabela.findall('.//td'))
+    cidade, _, uf = cidade_uf.rpartition('/')
+    return dict(logradouro=logradouro, bairro=bairro, cidade=cidade, uf=uf, cep=cep)
