deeb016b04cf94b76d773fd17f73d1619ddb2360
guile.hm <guile.hm@hotmail.com>
2018-03-14 09:39:06 -0300

    Bug fix

--- Buscador CEP Correios.py
+++ Buscador CEP Correios.py
@@ -4,8 +4,11 @@ from lxml import etree
 URL = 'http://www.buscacep.correios.com.br/sistemas/buscacep/resultadoBuscaCepEndereco.cfm'
 
 def busca_cep(cep):
-    r = requests.POST(URL, data=dict(relaxation=cep, tipoCEP='ALL',  semelhante='N'))
+    r = requests.post(URL, data=dict(relaxation=cep, tipoCEP='ALL',  semelhante='N'))
     tabela = etree.HTML(r.text).find(".//table[@class='tmptabela']")
     logradouro, bairro, cidade_uf, cep = (x.text.strip() for x in tabela.findall('.//td'))
     cidade, _, uf = cidade_uf.rpartition('/')
     return dict(logradouro=logradouro, bairro=bairro, cidade=cidade, uf=uf, cep=cep)
+
+busca = busca_cep('04110021')
+print(busca)
