c1aff1c19902c8c2366d802d7e66a9a157277b2b
guile.hm <guile.hm@hotmail.com>
2018-03-15 19:38:12 -0300

    dll files removed

--- Django Classes/virtual_env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\guilherme\documents\github\day-to-day\python\django classes\virtual_env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
