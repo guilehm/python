43f87430784eca270b25db526b5c8490ddffdbb1
guile.hm <guile.hm@hotmail.com>
2018-03-16 13:28:00 -0300

    Classes

---
+++ Classes.py
@@ -0,0 +1,8 @@
+class Personagem:
+    def __init__(self, nome, vida, p_ataque, p_defesa, arma, frase):
+        self.nome = nome
+        self.vida = vida
+        self.p_ataque = p_ataque
+        self.p_defesa = p_defesa
+        self.arma = arma
+        self.frase = frase
