df2193c85f0cd788d51e18309a1bd370a72c9231
guile.hm <guile.hm@hotmail.com>
2018-03-16 13:30:37 -0300

    Stats

--- Classes.py
+++ Classes.py
@@ -6,3 +6,11 @@ class Personagem:
         self.p_defesa = p_defesa
         self.arma = arma
         self.frase = frase
+
+    def stats(self):
+        print(f'Nome: {self.nome}')
+        print(f'{self.nome} diz: {self.frase}')
+        print(f'HP: {self.vida}')
+        print(f'Arma: {self.vida}')
+        print(f'Ataque: {self.vida}')
+        print(f'Defesa: {self.vida}')
