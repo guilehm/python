d8c2feaa78edcea0e213dc615ca4fe6356c1a65e
guile.hm <guile.hm@hotmail.com>
2018-03-16 17:05:30 -0300

    __str__

--- Classes.py
+++ Classes.py
@@ -8,9 +8,10 @@ class Personagem:
         self.p_defesa = p_defesa
         self.arma = arma
         self.frase = frase
+
         self.vivo = True
         self.hit = round((self.p_ataque / 4) * uniform(0.9, 1.0), 2)
-        self.defesa = round(self.p_defesa * 0.1, 2)
+        self.defesa = round(self.p_defesa * 0.08, 2)
 
     def stats(self):
         print(f'\nNome: {self.nome}')
@@ -20,9 +21,20 @@ class Personagem:
         print(f'Ataque: {self.p_ataque}')
         print(f'Defesa: {self.p_defesa}')
 
+    # def ataque(self, object):
+    #     dano = self.hit - object.defesa
+    #     print(f'{self}')
 
-
+    def __str__(self):
+        return self.nome
 
 batman = Personagem('Batman', 100, 80, 90, 'Boomerangue', 'Eu sou o BATMAN!')
+print(batman.hit)
+print(batman.defesa)
 
 superman = Personagem('Super-Man', 85, 85, 95, 'Raio Laser', 'Ao infinito e além!')
+print(superman.hit)
+print(superman.defesa)
+
+print(batman)
+print(superman)
