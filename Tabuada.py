47ebc32ad9e467ac977871a1a90593735b0fdf70
guile.hm <guile.hm@hotmail.com>
2018-04-05 22:38:24 -0300

    helping friends

--- Tabuada.py
+++ Tabuada.py
@@ -1,5 +1,5 @@
 # Tabuada em uma linha
-tabuada = [(a, b, a * b) for a in range(2, 3) for b in range(1, 11)]
+# tabuada = [(a, b, a * b) for a in range(2, 3) for b in range(1, 11)]
 
 # print(tabuada)
 print("\n".join("{} x {} = {}".format(a, b, a * b) for a in range(1, 11) for b in range(1, 11)))
---
+++ Tests/exercicio_cidadePais.py
@@ -0,0 +1,4 @@
+def get_city_country(city, country):
+    """ Gera um nome de uma cidade e do pais. """
+    city_country = city + ", " + country
+    return city_country.title()
