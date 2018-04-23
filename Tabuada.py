040003f5541a7110f97240be3c1e5e4c9b33fa81
guile.hm <guile.hm@hotmail.com>
2018-04-23 17:24:17 -0300

    tabuada

--- Tabuada.py
+++ Tabuada.py
@@ -2,4 +2,8 @@
 # tabuada = [(a, b, a * b) for a in range(2, 3) for b in range(1, 11)]
 
 # print(tabuada)
-print("\n".join("{} x {} = {}".format(a, b, a * b) for a in range(1, 11) for b in range(1, 11)))
+# print("\n".join("{} x {} = {}".format(a, b, a * b) for a in range(1, 11) for b in range(1, 11)))
+
+num = int(input('Digite um número: \n'))
+for i in range(1,11):
+    print('{} x {} = {}'.format(num, i, num*i))
