90518e2e77b6bd59e02e05313344e0e12e6d9ef3
guile.hm <guile.hm@hotmail.com>
2018-04-29 11:40:01 -0300

    Squares and Remainders

--- List Comprehensions.py
+++ List Comprehensions.py
@@ -1,1 +1,5 @@
-print([('{} ^2 = {}'.format(a, a**2)) for a in range(1,11)])
+squares = ([('{} ^2 = {}'.format(a, a**2)) for a in range(1,11)])
+print(squares)
+
+remainders = [i**2 % 5 for i in range(1,101)]
+print(remainders)
