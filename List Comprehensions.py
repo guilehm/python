7126e25aab3249b58cd44a3230fe980f5fa1d6ac
guile.hm <guile.hm@hotmail.com>
2018-04-29 11:44:05 -0300

    Table

--- List Comprehensions.py
+++ List Comprehensions.py
@@ -1,5 +1,8 @@
 squares = ([('{} ^2 = {}'.format(a, a**2)) for a in range(1,11)])
 print(squares)
 
-remainders = [i**2 % 5 for i in range(1,101)]
+remainders = [i**2 % 15 for i in range(1,51)]
 print(remainders)
+
+table = ['{} x {} = {}'.format(a, b, a*b) for a in range(1,11) for b in range(1,11)]
+print(table)
