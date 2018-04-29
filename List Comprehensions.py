66cae71ae43a1d4d21f30edcfef92dfcf292d1de
guile.hm <guile.hm@hotmail.com>
2018-04-29 12:34:06 -0300

    Lista

--- List Comprehensions.py
+++ List Comprehensions.py
@@ -1,8 +1,12 @@
 squares = ([('{} ^2 = {}'.format(a, a**2)) for a in range(1,11)])
-print(squares)
+# print("".join(squares))
 
 remainders = [i**2 % 15 for i in range(1,51)]
-print(remainders)
+# print(remainders)
+
 
 table = ['{} x {} = {}'.format(a, b, a*b) for a in range(1,11) for b in range(1,11)]
-print(table)
+print("\n".join(table))
+
+lista = ['{},{},{}'.format(a, b, c) for a in range(1, 4) for b in range (1, 4) for c in range(1, 4)]
+print(']\n['.join(lista) + ']')
