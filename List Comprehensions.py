22fd75cd3c330d9cac86e1917a8eac82266b7960
guile.hm <guile.hm@hotmail.com>
2018-04-30 20:03:30 -0300

    List Comprehension

--- List Comprehensions.py
+++ List Comprehensions.py
@@ -14,9 +14,5 @@ lista = ['{},{},{}'.format(a, b, c) for a in range(1, 4) for b in range (1, 4) f
 table2 = ['{:2} x {:2} = {:3}\n{}'.format(a, b, a*b,('-'*13)) if b == 10 else '{:2} x {:2} = {:3}'.format(a, b, a*b) for a in range(1,11) for b in range(1,11)]
 print("\n".join(table2))
 
-for i in range(1,6):
-    for b in range(1,6):
-        print('{} ^ {} = {}'.format(i,b, i**b))
-
 numbers = ['{} ^ {} = {}'.format(a, b, a**b) for a in range(2,6) for b in range(1,6)]
 print('\n'.join(numbers))
