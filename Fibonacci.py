2f1fe61ff2a0e1d26669d6203af28e538dedc1d3
guile.hm <guile.hm@hotmail.com>
2018-05-06 20:44:01 -0300

    Fibonacci 2

--- Fibonacci.py
+++ Fibonacci.py
@@ -6,3 +6,13 @@ def fibonacci(n):
 
 for x in range(10):
     print(fibonacci(x), end=" ")
+
+print()
+def fibonacci2(n):
+    a,b = 0,1
+    for i in range(n):
+        a,b = b,a+b
+    return a
+
+for x in range(10):
+    print(fibonacci2(x), end=" ")
