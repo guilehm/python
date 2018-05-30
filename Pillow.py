784052fc61f1e741dad7eb54f816bd77a5185043
guile.hm <guile.hm@hotmail.com>
2018-05-29 23:04:34 -0300

    Using ImageDraw

--- Pillow.py
+++ Pillow.py
@@ -1,4 +1,6 @@
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
+import os
+
 
 frederico = Image.open('dog.jpg')
 print(frederico)
@@ -8,3 +10,13 @@ print(dir(frederico))
 print(frederico.filename)
 print(frederico.format)
 print(frederico.format_description)
+
+frederico.rotate(90).save('dog_rotate90.jpg')
+
+im = Image.new('RGBA', (200, 200), 'white')
+draw = ImageDraw.Draw(im)
+draw.text((20, 150), 'Hello', fill='purple')
+fonts_folder = 'FONT_FOLDER'
+arial_font = ImageFont.truetype(os.path.join(fonts_folder, 'arial.ttf'), 32)
+draw.text((100, 150), 'Howdy', fill='gray', font=arial_font)
+im.save('text.png')
