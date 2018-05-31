dec29c4472a31b83580a411f4c00735fc73640a9
Guilherme Luis <guile.hm@hotmail.com>
2018-05-31 15:20:45 -0300

    Print mouse position

---
+++ mouse-now.py
@@ -0,0 +1,9 @@
+import pyautogui
+
+try:
+    while True:
+        x, y = pyautogui.position()
+        position_str = 'X:' + str(x).rjust(4) + ' Y:' + str(y).rjust(4)
+        print(position_str)
+except:
+    pass
