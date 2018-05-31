75d8cdcbff89fa254c980390ba125fb121ca22c7
Guilherme Luis <guile.hm@hotmail.com>
2018-05-31 14:53:53 -0300

    Create requests

--- web.py
+++ web.py
@@ -2,10 +2,12 @@ import requests
 
 res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
 print(type(res))
-
 print(res.status_code == requests.codes.ok)
-
 print(len(res.text))
-
 print(res.text[:250])
 
+import bs4
+
+res = requests.get('http://nostarch.com')
+res.raise_for_status()
+no_starch_soup = bs4.BeautifulSoup(res.text, "html.parser")
