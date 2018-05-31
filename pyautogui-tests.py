bb07b9e0728dd36ff39eb0a6be58b0160efa11ad
Guilherme Luis <guile.hm@hotmail.com>
2018-05-31 15:12:05 -0300

    Moving mouse inse a loop for

--- pyautogui-tests.py
+++ pyautogui-tests.py
@@ -8,3 +8,9 @@ pyautogui.moveTo(100, 100, duration=0.25)
 pyautogui.moveTo(800, 100, duration=0.25)
 pyautogui.moveTo(400, 400, duration=0.25)
 pyautogui.moveTo(100, 400, duration=0.25)
+
+for i in range(10):
+    pyautogui.moveRel(100, 0, duration=0.25)
+    pyautogui.moveRel(0, 100, duration=0.25)
+    pyautogui.moveRel(-100, 0, duration=0.25)
+    pyautogui.moveRel(0, -100, duration=0.25)
