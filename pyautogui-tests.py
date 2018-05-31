124ddb226759feb91e4d0b336a736c4f96320744
Guilherme Luis <guile.hm@hotmail.com>
2018-05-31 15:08:54 -0300

    Movign mouse

---
+++ pyautogui-tests.py
@@ -0,0 +1,6 @@
+import pyautogui
+
+pyautogui.moveTo(100, 100, duration=0.25)
+pyautogui.moveTo(200, 100, duration=0.25)
+pyautogui.moveTo(200, 200, duration=0.25)
+pyautogui.moveTo(100, 200, duration=0.25)
