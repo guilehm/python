69a74c2db7ad6258c046f5b09af4eb5f437e1eca
Guilherme Luis <guile.hm@hotmail.com>
2018-06-24 13:02:01 -0300

    Print sorted otc by values

--- Air Incidents.py
+++ Air Incidents.py
@@ -1,4 +1,5 @@
 import csv
+import operator
 
 from collections import Counter
 
@@ -8,11 +9,19 @@ filename = 'files/oco.csv'
 reader = csv.DictReader(open(filename))
 
 fields = reader.fieldnames
+print(fields)
+
 codes = []
+ocurrencies_type = []
 
 for row in reader:
     codes.append(row['codigo_ocorrencia'])
+    ocurrencies_type.append(row['ocorrencia_tipo'])
 
 codes_counter = dict(Counter(codes))
-print(codes_counter)
+ocurrencies_type_counter = dict(Counter(ocurrencies_type))
 
+otc = ocurrencies_type_counter
+sorted_otc_by_value = sorted(otc.items(), key=lambda kv: kv[1])
+
+print(sorted_otc_by_value)
