838d6a29fa8005322fe9802b66ad44d22ede720e
Guilherme Luis <guile.hm@hotmail.com>
2018-07-03 20:20:32 -0300

    Create Thread.py

---
+++ Thread.py
@@ -0,0 +1,23 @@
+import time
+import threading
+
+
+def print1():
+    print('1')
+    time.sleep(1)
+    return print1()
+
+def print2():
+    print('2')
+    time.sleep(1)
+    return print2()
+
+
+def main():
+    threading.Thread(target=print1).start()
+    threading.Thread(target=print2).start()
+
+if __name__ == '__main__':
+    main()
+
+    
