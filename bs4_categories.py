21e4dd6014f5b27189bdb259f5be8d23a3b06e0d
Guilherme Luis <guile.hm@hotmail.com>
2018-08-04 15:55:03 -0300

    Create csv file

--- bs4_categories.py
+++ bs4_categories.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 r = requests.get('https://www.guiadecompra.com/categorias/')
 soup = BeautifulSoup(r.content, 'html.parser')
@@ -7,3 +8,11 @@ soup = BeautifulSoup(r.content, 'html.parser')
 print('Title: ', soup.title.get_text())
 h2s = soup.find_all('h2')
 categories = [h2.text for h2 in h2s]
+
+print(categories)
+
+with open('files/categories.csv', 'w') as csv_file:
+    writer = csv.DictWriter(csv_file, fieldnames=['Categorias'])
+    writer.writeheader()
+    for category in categories:
+        writer.writerow({'Categorias': category})
