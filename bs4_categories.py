d76905024a4abf9d1d752b9943322ad96032702f
Guilherme Luis <guile.hm@hotmail.com>
2018-08-04 17:17:25 -0300

    Use zip_longest

--- bs4_categories.py
+++ bs4_categories.py
@@ -1,18 +1,28 @@
-from bs4 import BeautifulSoup
 import requests
 import csv
+
+from bs4 import BeautifulSoup
+from itertools import zip_longest
 
 r = requests.get('https://www.guiadecompra.com/categorias/')
 soup = BeautifulSoup(r.content, 'html.parser')
 
 print('Title: ', soup.title.get_text())
 h2s = soup.find_all('h2')
-categories = [h2.text for h2 in h2s]
+sbtcats = soup.find_all('a', {'class': 'sbtcat'})
 
-print(categories)
+categories = [{'Categorias': h2.text} for h2 in h2s]
+sub_categories = [{'Sub Categorias': sbtcat.text} for sbtcat in sbtcats]
+
+categories = [h2.text for h2 in h2s]
+sub_categories = [sbtcat.text for sbtcat in sbtcats]
+
+rows = zip_longest(*([categories, sub_categories]), fillvalue='')
 
 with open('files/categories.csv', 'w') as csv_file:
-    writer = csv.DictWriter(csv_file, fieldnames=['Categorias'])
+    fieldnames = ['Categorias', 'Sub Categorias']
+    writer = csv.DictWriter(csv_file, delimiter=',', fieldnames=fieldnames)
     writer.writeheader()
-    for category in categories:
-        writer.writerow({'Categorias': category})
+
+    for cat, sub in rows:
+        writer.writerow({'Categorias': cat, 'Sub Categorias': sub})
