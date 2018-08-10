c8607741cc3c865e99f31720cf461c826c267ca4
Guilherme Luis <guile.hm@hotmail.com>
2018-08-09 22:13:40 -0300

    Black Python

--- Fibonacci.py
+++ Fibonacci.py
@@ -2,17 +2,21 @@ def fibonacci(n):
     if n < 2:
         return n
     else:
-        return fibonacci(n-1)+fibonacci(n-2)
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
 
 for x in range(10):
     print(fibonacci(x), end=" ")
 
 print()
+
+
 def fibonacci2(n):
-    a,b = 0,1
+    a, b = 0, 1
     for i in range(n):
-        a,b = b,a+b
+        a, b = b, a + b
     return a
+
 
 for x in range(10):
     print(fibonacci2(x), end=" ")
