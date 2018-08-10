75f28cd390a8a548037f545d9a34abe345c966dc
Guilherme Luis <guile.hm@hotmail.com>
2018-08-09 21:58:34 -0300

    Code improvements

--- BS4_ibge.py
+++ BS4_ibge.py
@@ -7,12 +7,11 @@ r = requests.get('https://ww2.ibge.gov.br/home/estatistica/indicadores/precos/in
 
 soup = BeautifulSoup(r.content, 'html.parser')
 
-print('Title: ', soup.title.get_text())
-print('Last IPCA Index:')
 bs = soup.find_all('b')
-
 last_ipca = bs[3]
-print(last_ipca.get_text())
+
+print('Title: ', soup.title.get_text())
+print('Last IPCA Index:', (last_ipca.get_text()))
 
 r = requests.get('https://www2.cetip.com.br/ConsultarTaxaDi/ConsultarTaxaDICetip.aspx')
 soup = BeautifulSoup(r.content, 'html.parser')
