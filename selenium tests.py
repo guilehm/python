a134c157aff31a86b014e01bf790833d9a28b59c
Guilherme Luis <guile.hm@hotmail.com>
2018-08-29 21:06:40 -0300

    Add selenium tests

--- selenium tests.py
+++ selenium tests.py
@@ -1,11 +1,22 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common import action_chains
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 chrome = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')
-
+print(dir(chrome))
 chrome.get('https://www.zerocarbo.com.br/')
+action = action_chains.ActionChains(chrome)
 
 vitaminas = chrome.find_element_by_xpath(
     '//*[@id="corpo"]/div/div[1]/div[1]/div[1]/div/ul/li[5]/ul/li[18]/a'
 )
 
 vitaminas.click()
+
+
+zma = chrome.find_element_by_xpath(
+    '//div/ul/li/ul/li/a[contains(@title, "ZMA")]'
+)
