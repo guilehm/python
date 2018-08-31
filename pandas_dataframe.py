84395f4aee8ac776345cfb5445680b173c268867
Guilherme Luis <guile.hm@hotmail.com>
2018-08-30 21:54:15 -0300

    Create new column dataframe

--- pandas_dataframe.py
+++ pandas_dataframe.py
@@ -5,3 +5,9 @@ import numpy as np
 np.random.seed(101)
 df = pd.DataFrame(np.random.randn(5, 4), index='A B C D E'.split(), columns='W X Y Z'. split())
 print(df)
+
+print(df[['W', 'Z']])
+
+df['new'] = df['W'] + df['X']
+
+print(df)
