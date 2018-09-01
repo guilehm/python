e853aff416b137d7459a319694baa675cdc0f6fd
Guilherme Luis <guile.hm@hotmail.com>
2018-09-01 19:28:05 -0300

    Fix imports

--- pandas_dataframe.py
+++ pandas_dataframe.py
@@ -1,6 +1,5 @@
-import pandas as pd
 import numpy as np
-
+import pandas as pd
 
 np.random.seed(101)
 df = pd.DataFrame(np.random.randn(5, 4), index='A B C D E'.split(), columns='W X Y Z'. split())
