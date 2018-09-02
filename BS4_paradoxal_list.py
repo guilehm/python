0f476ea63675605f66cd19b0351f74a1de4f19b7
Guilherme Luis <guile.hm@hotmail.com>
2018-09-02 16:40:34 -0300

    Print paradox kinds

--- BS4_paradoxal_list.py
+++ BS4_paradoxal_list.py
@@ -1,10 +1,11 @@
-import csv
-from collections import defaultdict
-
 import requests
 from bs4 import BeautifulSoup
 
 r = requests.get("https://pt.wikipedia.org/wiki/Lista_de_paradoxos")
 soup = BeautifulSoup(r.content, "html.parser")
 
+div = soup.find("div", class_="mw-parser-output")
 paradox_kinds = [paradox.text for paradox in soup.find_all(class_="mw-headline")]
+
+for kind in paradox_kinds:
+    print(kind)
