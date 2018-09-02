908a82c8071c5c010bf27c43aa3e0a1407fda993
Guilherme Luis <guile.hm@hotmail.com>
2018-09-02 11:28:31 -0300

    Create categories csv file

---
+++ BS4_categories.py
@@ -0,0 +1,30 @@
+import csv
+
+import requests
+from bs4 import BeautifulSoup
+from collections import defaultdict
+
+
+r = requests.get("https://www.guiadecompra.com/categorias/")
+soup = BeautifulSoup(r.content, "html.parser")
+
+div = soup.find('div', class_='baseB')
+
+categories = defaultdict(list)
+category = None
+
+for child in div.children:
+    if child.name == 'h2':
+        category = child.text
+        continue
+
+    categories[category].append(child.li.a.text)
+
+with open("files/categories_2.csv", "w") as csv_file:
+    fieldnames = ["categories", "sub categories"]
+    writer = csv.DictWriter(csv_file, delimiter=",", fieldnames=fieldnames)
+    writer.writeheader()
+
+    for cat, subs in categories.items():
+        for sub in subs:
+            writer.writerow({"categories": cat, "sub categories": sub})
