1df4212be3ce238988379354229e74904cf08451
Guilherme Luis <guile.hm@hotmail.com>
2018-09-03 21:08:00 -0300

    Use etree

--- BS4_paradoxal_list.py
+++ BS4_paradoxal_list.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from lxml import etree
 
 r = requests.get("https://pt.wikipedia.org/wiki/Lista_de_paradoxos")
 soup = BeautifulSoup(r.content, "html.parser")
@@ -9,3 +10,9 @@ paradox_kinds = [paradox.text for paradox in soup.find_all(class_="mw-headline")
 
 for kind in paradox_kinds:
     print(kind)
+
+print("=" * 90)
+
+root = etree.HTML(r.content)
+for x in root.findall(".//span[@class='toctext']"):
+    print(x.text)
