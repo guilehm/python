15e91795b7836ff3007a56148136546609cbe215
Guilherme Luis <guile.hm@hotmail.com>
2018-11-06 21:40:56 -0200

    Create french deck file

---
+++ fluent-python/french-deck.py
@@ -0,0 +1,60 @@
+import collections
+from random import choice
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
+
+
+beer_card = Card('7', 'diamonds')
+
+deck = FrenchDeck()
+len(deck)  # __len__
+deck[0]  # __getitem__
+
+choice(deck)
+choice(deck)
+
+deck[12::13]  # begin at 12 index - step 13
+
+for card in deck:  # __getitem__ makes deck iterable
+    # print(card)
+    pass
+
+Card('Q', 'hearts') in deck   # True
+Card('Q', 'beasts') in deck   # False
+
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
+
+
+for card in sorted(deck, key=spades_high):
+    # print(card)
+    pass
+
+
+
+
+
+
+
+
+
+
+
