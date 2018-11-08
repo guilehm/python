ce15b8df489c73dfcd7a66f698e32cefd8e5d1a6
Guilherme Luis <guile.hm@hotmail.com>
2018-11-07 23:59:40 -0200

    Rename file and improve code

--- fluent-python/listcomps_map_filter_unpacking.py
+++ fluent-python/listcomps_map_filter_unpacking.py
@@ -38,8 +38,8 @@ metro_areas = [
     ('New York-Newark', 'US', 20.104, (40.808611, -74.020386)),
     ('Sao Paulo', 'BR', 19.649, (-23.547778, -46.635833)),
 ]
-print('{:15} | {:^9} | {:^9}'.format('', 'lat.', 'long.'))
-fmt = '{:15} | {:9.4f} | {:9.4f}'
+print('{:15} | {:^9} | {:^9} |'.format('', 'lat.', 'long.'))
+fmt = '{:15} | {:9.4f} | {:9.4f} |'
 for name, cc, pop, (latitude, longitude) in metro_areas:
     # if longitude <= 0:
     print(fmt.format(name, latitude, longitude))
