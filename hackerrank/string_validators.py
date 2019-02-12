aa0de3bbeecb6f86a2dc0ae0939041d659c33e12
Guilherme Luis <guile.hm@hotmail.com>
2019-02-11 22:15:55 -0200

    create string validators challenge file

---
+++ hackerrank/string_validators.py
@@ -0,0 +1,9 @@
+# https://www.hackerrank.com/challenges/string-validators/problem
+
+if __name__ == '__main__':
+    s = input()
+    print(any(i.isalnum() for i in s))
+    print(any(i.isalpha() for i in s))
+    print(any(i.isdigit() for i in s))
+    print(any(i.islower() for i in s))
+    print(any(i.isupper() for i in s))
